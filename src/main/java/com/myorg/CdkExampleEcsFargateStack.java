7d31cb5872d3cbe83ab1ba1967a2c10818d4df55
igueyetraining <ismaelgueyetraining@gmail.com>
2025-05-06 20:29:31 +0000

    Create repository and upload image from DockerHub

--- src/main/java/com/myorg/CdkExampleEcsFargateStack.java
+++ src/main/java/com/myorg/CdkExampleEcsFargateStack.java
@@ -2,9 +2,15 @@ package com.myorg;
 
 import java.util.List;
 
+import io.github.cdklabs.cdk.ecr.deployment.DockerImageName;
+import io.github.cdklabs.cdk.ecr.deployment.ECRDeployment;
+import io.github.cdklabs.cdk.ecr.deployment.ECRDeploymentProps;
 import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecr.RepositoryProps;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
@@ -14,8 +20,6 @@ import software.amazon.awscdk.services.iam.ManagedPolicy;
 import software.amazon.awscdk.services.iam.Role;
 import software.amazon.awscdk.services.iam.ServicePrincipal;
 import software.constructs.Construct;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class CdkExampleEcsFargateStack extends Stack {
     public CdkExampleEcsFargateStack(final Construct scope, final String id) {
@@ -25,25 +29,24 @@ public class CdkExampleEcsFargateStack extends Stack {
     public CdkExampleEcsFargateStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
+        String accountIdFromEnv =  this.getAccount();
+    
+        new Repository(this, "MyDestinationRepository", 
+            RepositoryProps.builder().repositoryName("userbook-maven").removalPolicy(RemovalPolicy.DESTROY).build());
+            
+        String dockerHubImageName = "ismaelgueyetraining/userbook-maven:0.0.3-SNAPSHOT";
+        String ecrDockerImageName = String.format("%s.dkr.ecr.us-east-1.amazonaws.com/userbook-maven:latest", accountIdFromEnv);
+        
+        ECRDeployment.Builder.create(this, "DeployDockerImage2")
+         .src(new DockerImageName(dockerHubImageName))
+         .dest(new DockerImageName(ecrDockerImageName))
+         .build();
+        
+
         Vpc vpc = Vpc.Builder.create(this, "MyVpcForEcsDemo").maxAzs(3).build();
 
         Cluster cluster = Cluster.Builder.create(this, "MyClusterForEcsDemo")
                 .vpc(vpc).build();
-                
-        String accountIdFromEnv =  this.getAccount();
-        
-//        TaskDefinition taskDefinition = new TaskDefinition(scope, id, null);
-//        taskDefinition.getExecutionRole().addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ContainerRegistryPowerUser"));
-
-//        ContainerDefinition container = taskDefinition.addContainer("MyContainer", ContainerDefinition.Builder.create()
-//            .image(ContainerImage.fromRegistry("my-image"))
-//            .build());
-//
-//        container.addPortMappings(PortMapping.builder()
-//            .containerPort(8080)  // The port the application listens on in the container
-//            .hostPort(8080)       // The port the host listens on
-//            .build());
-
         // Define the execution role
         Role executionRole = Role.Builder.create(this, "TaskExecutionRole")
                 .assumedBy(new ServicePrincipal("ecs-tasks.amazonaws.com"))
@@ -52,15 +55,13 @@ public class CdkExampleEcsFargateStack extends Stack {
                 ))
                 .build();
                 
-        
-        
         ApplicationLoadBalancedFargateService loadBalancedFargateService = ApplicationLoadBalancedFargateService.Builder.create(this, "MyFargateService")
                 .cluster(cluster)
                 .cpu(512)
                 .desiredCount(2)
                 .taskImageOptions(
                        ApplicationLoadBalancedTaskImageOptions.builder()
-                               .image(ContainerImage.fromRegistry(accountIdFromEnv + ".dkr.ecr.us-east-1.amazonaws.com/userbook-maven:latest"))
+                               .image(ContainerImage.fromRegistry(ecrDockerImageName))
                                .containerPort(8081)
                                .executionRole(executionRole)
                                .build())
@@ -71,5 +72,8 @@ public class CdkExampleEcsFargateStack extends Stack {
         loadBalancedFargateService.getTargetGroup().configureHealthCheck(HealthCheck.builder()
             .path("/userbook/hello") // Set your desired health check path here
             .build());
+            
+    
+    
     }
 }
